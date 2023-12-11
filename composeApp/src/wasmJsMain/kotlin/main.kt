a927303290ee1c70bc4eedcf962ac4c169d92fd1
Akexorcist <akexorcist@gmail.com>
2023-12-12 03:59:01 +0700

    Add screen size for web

--- composeApp/src/wasmJsMain/kotlin/main.kt
+++ composeApp/src/wasmJsMain/kotlin/main.kt
@@ -1,7 +1,16 @@
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.CanvasBasedWindow
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    CanvasBasedWindow(canvasElementId = "ComposeTarget") { App() }
+    CanvasBasedWindow(canvasElementId = "ComposeTarget") {
+        App(
+            windowSize = DpSize(
+                width = 1920.dp,
+                height = 1080.dp,
+            )
+        )
+    }
 }
