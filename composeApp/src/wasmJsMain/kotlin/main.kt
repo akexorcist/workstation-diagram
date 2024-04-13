57ebae3587f2acab2fc5d67ef6b2c7ffabdfbde1
Akexorcist <akexorcist@gmail.com>
2024-04-14 01:28:07 +0700

    Use container size as window size

--- composeApp/src/wasmJsMain/kotlin/main.kt
+++ composeApp/src/wasmJsMain/kotlin/main.kt
@@ -1,4 +1,5 @@
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.CanvasBasedWindow
@@ -6,10 +7,11 @@ import androidx.compose.ui.window.CanvasBasedWindow
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     CanvasBasedWindow(canvasElementId = "WorkstationDiagram") {
+        val windowSize = LocalWindowInfo.current.containerSize
         App(
             windowSize = DpSize(
-                width = 1280.dp,
-                height = 720.dp,
+                width = windowSize.width.dp,
+                height = windowSize.height.dp,
             )
         )
     }
